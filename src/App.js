bcba07fe5764675736051801d3ed96c5d81dfd7f
green <greenn.lab@gmail.com>
2021-05-17 22:16:41 +0900

    update// Todo component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Todo from './components/Todo'
+
 function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn react" />
+      <Todo text="Hello" />
+      <Todo text={'Hi'} />
+    </div>
+  )
 }
 
 export default App
